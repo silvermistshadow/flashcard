758f609cb65c2604c2f704f23e44c4cb7a4c4ddf
silvermistshadow <bmead72@gmail.com>
2019-02-04 09:58:42 -0800

    add the rest of the flashcards I feel like doing for now

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,18 @@ $(document).ready(function(){
   $("#card0-init, #card-0").click(function(){
     $("#card0-init, #card-0").slideToggle();
   });
+  $("#card1-init, #card-1").click(function(){
+    $("#card1-init, #card-1").slideToggle();
+  });
+
+  $("#card2-init, #card-2").click(function(){
+    $("#card2-init, #card-2").slideToggle();
+  });
+
+  $("#card3-init, #card-3").click(function(){
+    $("#card3-init, #card-3").slideToggle();
+  });
+
+
 
 });
